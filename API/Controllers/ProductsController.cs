843bce4552434d53838e17051c0c658d2e07cdad
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-08 12:22:50 +0200

    Basic Controller Impl

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.dao;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +14,28 @@ namespace API.Controllers
     public class ProductsController : ControllerBase
     {
         
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context) {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetProducts() {
+
+            var result = await context.Products.ToListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetProduct([FromRoute] int id) {
+
+            var result = await context.Products
+                .SingleOrDefaultAsync(p => p.Id == id);
+
+            return Ok(result);
+        }
+
     }
 }
