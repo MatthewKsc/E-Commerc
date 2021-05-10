70c464f222e8d1d3b7aeca160e8d00bcb2014845
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-10 22:16:19 +0200

    Crated basic error handling

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using API.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("errors/{code}")]
+    public class ErrorController : ControllerBase
+    {
+        public IActionResult Error(int code) {
+            return new ObjectResult(new ApiResponse(code));
+        }
+    }
+}
---
+++ API/Exceptions/ApiResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Exceptions
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public ApiResponse(int statusCode, string message = null) {
+            StatusCode = statusCode;
+            Message = message ?? GedDefaultMessageForStatusCode(statusCode);
+        }
+
+        private string GedDefaultMessageForStatusCode(int statusCode) {
+            return statusCode switch {
+                400 => "A bad request, you gave made",
+                401 => "Authorized, you are not",
+                404 => "Resource found, it was not",
+                500 => "Server internal Error",
+                _ => null
+            };
+        }
+    }
+}
