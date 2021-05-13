dcdf1eef523bc0a70dc47a8d806add703fc3dcd2
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-13 22:50:42 +0200

    Added BoostStrap and https to our Client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component ({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'client';
+  title = 'Skinet';
 }
