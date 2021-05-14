d345672fb68413d4f8ba9ab30d6c08d32f7c117e
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-14 12:59:46 +0200

    NavBar impl

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
