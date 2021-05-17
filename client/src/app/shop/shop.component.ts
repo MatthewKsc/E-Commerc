65bf294e161709df388da6faadc5f0445dee427c
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-17 19:16:36 +0200

    Added Brand, Types and Products to Client UI

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Brand } from '../models/brands';
 import { Product } from '../models/product';
+import { ProductType } from '../models/ProductType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -10,15 +12,38 @@ import { ShopService } from './shop.service';
 export class ShopComponent implements OnInit {
 
   products: Product[];
+  brands: Brand[];
+  types: ProductType[];
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts(){
     this.shopService.getProducts().subscribe(response => {
       this.products = response.items;
     }, error => {
       console.log(error);
-    })
+    });
   }
 
+  getBrands(){
+    this.shopService.getBrands().subscribe(response=>{
+      this.brands = response;
+    }, error=>{
+      console.log(error);
+    });
+  }
+
+  getTypes(){
+    this.shopService.getTypes().subscribe(response=>{
+      this.types = response;
+    }, error=>{
+      console.log(error);
+    });
+  }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Brand } from '../models/brands';
 import { Pagination } from '../models/pagination';
+import { ProductType } from '../models/ProductType';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,13 @@ export class ShopService {
 
   getProducts() {
     return this.http.get<Pagination>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getBrands(){
+    return this.http.get<Brand[]>(this.baseUrl + 'brands');
+  }
+
+  getTypes(){
+    return this.http.get<ProductType[]>(this.baseUrl + 'types');
   }
 }
