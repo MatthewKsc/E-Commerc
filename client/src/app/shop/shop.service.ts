75771b33355c7a2652ec2e65aba6e9df8186314c
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-17 20:41:09 +0200

    Added Impl of Filters in client

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Brand } from '../models/brands';
 import { Pagination } from '../models/pagination';
 import { ProductType } from '../models/ProductType';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,27 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get<Pagination>(this.baseUrl + 'products?pageSize=50');
+  getProducts(brandId?: number, typeId?: number, sort?: string) {
+    let params = new HttpParams();
+
+    if(brandId){
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if(typeId){
+      params = params.append('typeId', typeId.toString());
+    }
+
+    if(sort){
+      params = params.append('sort', sort);
+    }
+
+    return this.http.get<Pagination>(this.baseUrl + 'products', {observe: 'response', params})
+    .pipe(
+      map(response =>{
+        return response.body;
+      })
+    );
   }
 
   getBrands(){
