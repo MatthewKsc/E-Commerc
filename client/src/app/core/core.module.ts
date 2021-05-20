7cd711bcbe0faf40df257f47541c237c48037395
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-20 15:20:51 +0200

    Reinstall of ngx-toastr due to not compatiblie Angular 11 to toaster v14 and added  xng-breadcrumb to project

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,15 +5,19 @@ import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 @NgModule({
   declarations: [
     NavBarComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    SectionHeaderComponent
   ],
   imports: [
     CommonModule,
+    BreadcrumbModule,
     RouterModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
@@ -21,7 +25,8 @@ import { ToastrModule } from 'ngx-toastr';
     })
   ],
   exports: [
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent
   ]
 })
 export class CoreModule { }
