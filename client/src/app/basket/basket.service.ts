1902a90fa04f5fb591c3cb7aaef195414e96ab44
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-22 20:34:04 +0200

    Added Basket Service with Impl to Client UI

---
+++ client/src/app/basket/basket.service.ts
@@ -0,0 +1,79 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Basket, BaskteItem, IBasket } from '../models/basket';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasketService {
+
+  baseURL = environment.apiUrl;
+  private basketSource = new BehaviorSubject<IBasket>(null);
+  basket$ = this.basketSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  getBasket(id: string){
+    return this.http.get(this.baseURL +'basket?id='+id)
+      .pipe(
+        map((basket: IBasket) =>{
+          this.basketSource.next(basket);
+        })
+      );
+  }
+
+  setBasket(basket: IBasket){
+    return this.http.post(this.baseURL+'basket', basket).subscribe((response: IBasket)=>{
+      this.basketSource.next(response);
+      console.log(response);
+    }, error=>{
+      console.log(error);
+    });
+  }
+
+  getCurrentBasketValue(){
+    return this.basketSource.value;
+  }
+
+  addItemToBasket(item: Product, quantity = 1){
+    const itemToAdd: BaskteItem = this.mapProductItemToBasketItem(item, quantity);
+    const basket = this.getCurrentBasketValue() ?? this.createBaske();
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    
+    this.setBasket(basket);
+  }
+
+  private addOrUpdateItem(items: BaskteItem[], itemToAdd: BaskteItem, quantity: number): BaskteItem[] {
+    const index = items.findIndex(i => i.id === itemToAdd.id);
+    if(index === -1){
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    }else{
+      items[index].quantity += quantity;
+    }
+
+    return items;
+  }
+
+  private createBaske(): IBasket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+
+  private mapProductItemToBasketItem(item: Product, quantity: number): BaskteItem {
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      pictureURL: item.pictureURL,
+      quantity,
+      brand: item.productBrand,
+      type: item.productType
+    };
+  }
+}
---
+++ client/src/app/models/basket.ts
@@ -0,0 +1,21 @@
+import {v4 as uuidv4} from 'uuid';
+
+export interface IBasket {
+    id: string;
+    items: BaskteItem[];
+}
+
+export interface BaskteItem {
+    id: number;
+    productName: string;
+    price: number;
+    quantity: number;
+    pictureURL: string;
+    brand: string;
+    type: string;
+}
+
+export class Basket implements IBasket{
+    id = uuidv4();
+    items: BaskteItem[] = [];
+}
