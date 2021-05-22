1d595c5f5a2c6f1e2b8182187fee2e24c4e73618
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-22 14:43:59 +0200

    Basket Controller Impl

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository basketRepository;
+
+        public BasketController(IBasketRepository basketRepository) {
+            this.basketRepository = basketRepository;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetBasketById([FromQuery] string id) {
+            var basket = await basketRepository.GetBasketAsync(id);
+
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateBasekt(CustomerBasket basket) {
+            var updatedBasket = await basketRepository.UpdateBasketAsync(basket);
+
+            return Ok(updatedBasket);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBasekt([FromQuery] string id) {
+            await basketRepository.DeleteBasketAsync(id);
+            
+            return NoContent();
+        }
+    }
+}
