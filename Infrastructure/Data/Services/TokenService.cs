92d0e3d357034f3d6c0a64e530188e66ffc64fae
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-25 15:42:49 +0200

    Added Service to generate Token

---
+++ Infrastructure/Data/Services/TokenService.cs
@@ -0,0 +1,46 @@
+﻿using Core.Entities.Identity;
+using Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration config;
+        private readonly SymmetricSecurityKey key;
+
+        public TokenService(IConfiguration config) {
+            this.config = config;
+            key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"]));
+        }
+
+        public string CreateToken(AppUser user) {
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.DisplayName)
+            };
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptior = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds,
+                Issuer = config["Token:Issuer"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptior);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
