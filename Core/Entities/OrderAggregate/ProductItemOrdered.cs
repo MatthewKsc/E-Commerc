4569634744d8357e9088829b936ab806725fa09c
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-28 13:23:37 +0200

    Added Entities to Order Functionality

---
+++ Core/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class ProductItemOrdered
+    {
+       
+        public int ProductItemId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureURL { get; set; }
+
+        public ProductItemOrdered() {
+        }
+
+        public ProductItemOrdered(int productItemId, string productName, string pictureURL) {
+            ProductItemId = productItemId;
+            ProductName = productName;
+            PictureURL = pictureURL;
+        }
+
+    }
+}
